b3f2237abed8c5f666dae05944f66067bd167568
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-07-17 12:23:21 +0530

    Update ec2.tf

--- terraform-scripts/ec2.tf
+++ terraform-scripts/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "vprofileapp_instance" {
   ami           = "ami-0eeca1225b745d958"  
   instance_type = "t2.micro"              
   subnet_id     = aws_subnet.public_subnets[0].id
-  key_name      = "bitidea"             
+  key_name      = "devops-keypair"             
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.public_sg.id]
 
