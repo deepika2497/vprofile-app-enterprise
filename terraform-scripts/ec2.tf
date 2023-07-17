9898738374de85b4da2277729f604fe9bb9aec7d
deepika2497 <deepikakakade96@gmail.com>
2023-07-17 12:27:29 +0530

    added to jenkins file

--- terraform-scripts/ec2.tf
+++ terraform-scripts/ec2.tf
@@ -1,1 +1,12 @@
-/terraform-scripts/.terraform/*
+resource "aws_instance" "vprofileapp_instance" {
+  ami           = "ami-0eeca1225b745d958"  
+  instance_type = "t2.micro"              
+  subnet_id     = aws_subnet.public_subnets[0].id
+  key_name      = "devops-keypair"             
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+
+  tags = {
+    Name = "VprofileApp_Prod"
+  }
+}
