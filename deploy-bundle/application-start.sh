70e3e651fc06d887e45715d80e52ff78b77035f8
deepika2497 <deepikakakade96@gmail.com>
2023-10-18 13:56:10 +0530

    added apspec

--- deploy-bundle/application-start.sh
+++ deploy-bundle/application-start.sh
@@ -13,4 +13,19 @@
 #     echo "Tomcat failed to start."
 # fi
 
-docker run -d  --name vprofile_app -p 8080:8080 851481789693.dkr.ecr.ap-south-1.amazonaws.com/vprofile-qa:vprofileapp-%version%
+#docker run -d  --name vprofile_app -p 8080:8080 851481789693.dkr.ecr.ap-south-1.amazonaws.com/vprofile-qa:vprofileapp-%version%
+
+#!/bin/bash
+
+# Start Tomcat using systemctl
+sudo systemctl start tomcat9
+
+# Wait for Tomcat to start
+sleep 5
+
+# Check if Tomcat is running
+if systemctl is-active --quiet tomcat9; then
+    echo "Tomcat is running."
+else
+    echo "Tomcat failed to start."
+fi
--- deploy-bundle/validate-service.sh
+++ deploy-bundle/validate-service.sh
@@ -4,4 +4,10 @@
 #     echo "Tomcat failed to start."
 # fi
 
-docker ps | grep vprofile_app
+#docker ps | grep vprofile_app
+
+if systemctl is-active --quiet tomcat9; then
+    echo "Tomcat is running."
+else
+    echo "Tomcat failed to start."
+fi
