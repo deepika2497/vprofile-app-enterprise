9b96743deb95c587e9583956e8f5ac2137037432
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-10-19 16:36:05 +0530

    Update s3.tf

--- terraform-scripts/s3.tf
+++ terraform-scripts/s3.tf
@@ -1,11 +1,7 @@
-provider "aws" {
-  region = "eu-north-1"  # Set your desired AWS region here
-}
-
 resource "aws_s3_bucket" "v_app_prod_bucket" {
   bucket = "vprofileapp-bucket"
     
-  tags = {
+tags = {
     Name        = "v-app-prod-bucket"
     Environment = "Production"
   }
