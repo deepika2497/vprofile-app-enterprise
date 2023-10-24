505f885269ccbe4deb720116b3a0189e27f2303d
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-10-24 17:59:47 +0530

    Update application-start.sh

--- deploy-bundle/application-start.sh
+++ deploy-bundle/application-start.sh
@@ -18,14 +18,17 @@
 #!/bin/bash
 
 # Start Tomcat using systemctl
-sudo systemctl start tomcat9
+# sudo systemctl start tomcat9
 
 # Wait for Tomcat to start
-sleep 5
+# sleep 5
 
 # Check if Tomcat is running
-if systemctl is-active --quiet tomcat9; then
-    echo "Tomcat is running."
-else
-    echo "Tomcat failed to start."
-fi
+# if systemctl is-active --quiet tomcat9; then
+ #   echo "Tomcat is running."
+# else
+  #  echo "Tomcat failed to start."
+# fi
+#!/bin/bash
+
+docker run -d  --name vprofile_app -p 8080:8080 278607931101.dkr.ecr.eu-north-1.amazonaws.com/vprofile-qa:vprofileapp-%version%
