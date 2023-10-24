833c38e0af277cdf84ad2e360f8f10b370deef54
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-10-24 18:53:45 +0530

    Update after-install.sh

--- deploy-bundle/after-install.sh
+++ deploy-bundle/after-install.sh
@@ -1,3 +1,3 @@
 
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 278607931101.dkr.ecr.eu-north-1.amazonaws.com
-docker push 278607931101.dkr.ecr.eu-north-1.amazonaws.com/vprofile:%version%
+docker push 278607931101.dkr.ecr.eu-north-1.amazonaws.com/vprofile:${version}
