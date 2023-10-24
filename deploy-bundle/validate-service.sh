192c2cf0e64c4538fb0618444d53392a8436b7e3
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-10-24 18:00:52 +0530

    Update validate-service.sh

--- deploy-bundle/validate-service.sh
+++ deploy-bundle/validate-service.sh
@@ -6,8 +6,9 @@
 
 #docker ps | grep vprofile_app
 
-if systemctl is-active --quiet tomcat9; then
-    echo "Tomcat is running."
-else
-    echo "Tomcat failed to start."
-fi
+# if systemctl is-active --quiet tomcat9; then
+#    echo "Tomcat is running."
+# else
+#    echo "Tomcat failed to start."
+# fi
+docker ps | grep vprofile_app
